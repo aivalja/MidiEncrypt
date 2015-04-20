e97c0fffe7cd28d511728dfd3419173f79d9be49
Maiju the Fabulous <anssi.tenhunen@gmail.com>
2015-04-20 09:14:08 +0300

    encryppt to function

--- encryption.py
+++ encryption.py
@@ -5,25 +5,31 @@ def note(track, tick, length, velocity, pitch):
 	track.append(midi.NoteOffEvent(tick = tick+length, pitch = pitch))
 	return track
 
-with open ("text.txt", "r") as file:
-	text = file.read().lower()
+def encrypt(textName, midiName):
 
-pattern = midi.Pattern()
-track=midi.Track()
-pattern.append(track)
-letters = ["a","b","c","d","e","f","g","h","i", "j", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"," ",".",",","!","?", '"',"`","&","'", "-",":",";","*","(",")","[","]","{","}","+","\n","\t", "\xe2", "\x80", "\x99", "\xef", "\xbb", "\xbf"]
+    with open (textName + ".txt", "r") as file:
+        text = file.read().lower()
 
-for i in range(len(text)):
-	x = letters.index(text[i])
-	track = note(track, i, 1000, 100, x)
+    pattern = midi.Pattern()
+    track=midi.Track()
+    pattern.append(track)
+    letters = ["a","b","c","d","e","f","g","h","i", "j", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"," ",".",",","!","?", '"',"`","&","'", "-",":",";","*","(",")","[","]","{","}","+","\n","\t", "\xe2", "\x80", "\x99", "\xef", "\xbb", "\xbf"]
 
-track.append(midi.EndOfTrackEvent(tick=1))
-midi.write_midifile("text.mid",pattern)
-with open("midi.txt", "w") as file:
-	file.write(str(pattern))
+    for i in range(len(text)):
+        x = letters.index(text[i])
+        track = note(track, i, 1000, 100, x)
 
+    track.append(midi.EndOfTrackEvent(tick=1))
+    midi.write_midifile(midiName + ".mid",pattern)
+    
+    with open("midi.txt", "w") as file:
+        file.write(str(pattern))
+
+encrypt(text, text)
+"""
 secret = str(midi.read_midifile("text.mid"))
 #print str(midi.parse_track(midi.read_midifile("text.mid"), track))
 
 secretSplit = secret.split(",    ")
 print secret
+"""
